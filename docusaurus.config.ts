36106dc676304b9e1515179de38f73048a867421
Bruno PC <work@brunopc.net>
2026-03-29 17:54:02 -0400

    Theme improvements

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -64,7 +64,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/club-des-maitres-accueil.jpg',
     colorMode: {
       respectPrefersColorScheme: true,
     },
